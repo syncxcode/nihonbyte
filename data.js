cc374c71e987ee583668caccdc6f427d24c182fb
Farhan Abdurrahman <35007662+syncxcode@users.noreply.github.com>
2026-02-10 20:02:29 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,30 +1,8 @@
 const words = [
-  {
-    kanji: "書く",
-    hiragana: "かく",
-    arti: "menulis",
-    level: "N5",
-    type: "verb"
-  },
-  {
-    kanji: "食べる",
-    hiragana: "たべる",
-    arti: "makan",
-    level: "N5",
-    type: "verb"
-  },
-  {
-    kanji: "高い",
-    hiragana: "たかい",
-    arti: "tinggi/mahal",
-    level: "N5",
-    type: "i-adj"
-  },
-  {
-    kanji: "静か",
-    hiragana: "しずか",
-    arti: "tenang",
-    level: "N5",
-    type: "na-adj"
-  }
+  { kanji: "書く", arti: "menulis", level: "N5", type: "verb" },
+  { kanji: "食べる", arti: "makan", level: "N5", type: "verb" },
+  { kanji: "高い", arti: "tinggi/mahal", level: "N5", type: "i-adj" },
+  { kanji: "静か", arti: "tenang", level: "N5", type: "na-adj" },
+  { kanji: "行く", arti: "pergi", level: "N5", type: "verb" },
+  { kanji: "飲む", arti: "minum", level: "N5", type: "verb" }
 ];

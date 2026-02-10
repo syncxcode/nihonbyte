03a345049c0ea53d0c7f73cd20c59d7273950f9c
Farhan Abdurrahman <35007662+syncxcode@users.noreply.github.com>
2026-02-10 20:22:38 +0700

    Delete data.js

--- data.js
+++
@@ -1,8 +0,0 @@
-const words = [
-  { kanji: "書く", arti: "menulis", level: "N5", type: "verb" },
-  { kanji: "食べる", arti: "makan", level: "N5", type: "verb" },
-  { kanji: "高い", arti: "tinggi/mahal", level: "N5", type: "i-adj" },
-  { kanji: "静か", arti: "tenang", level: "N5", type: "na-adj" },
-  { kanji: "行く", arti: "pergi", level: "N5", type: "verb" },
-  { kanji: "飲む", arti: "minum", level: "N5", type: "verb" }
-];

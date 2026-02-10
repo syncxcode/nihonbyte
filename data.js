6ef83ecf7040e3d87292d9b6a2e22c39f0f99497
Farhan Abdurrahman <35007662+syncxcode@users.noreply.github.com>
2026-02-10 18:27:28 +0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,30 @@
+const words = [
+  {
+    kanji: "書く",
+    hiragana: "かく",
+    arti: "menulis",
+    level: "N5",
+    type: "verb"
+  },
+  {
+    kanji: "食べる",
+    hiragana: "たべる",
+    arti: "makan",
+    level: "N5",
+    type: "verb"
+  },
+  {
+    kanji: "高い",
+    hiragana: "たかい",
+    arti: "tinggi/mahal",
+    level: "N5",
+    type: "i-adj"
+  },
+  {
+    kanji: "静か",
+    hiragana: "しずか",
+    arti: "tenang",
+    level: "N5",
+    type: "na-adj"
+  }
+];

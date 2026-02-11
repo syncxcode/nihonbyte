4c52d2953058f1972aa36e7b63fd03e2878f3dec
Farhan Abdurrahman <35007662+syncxcode@users.noreply.github.com>
2026-02-11 18:00:17 +0700

    Update app.js

--- app.js
+++ app.js
@@ -45,3 +45,17 @@ document.addEventListener("DOMContentLoaded", () => {
   render();
 
 });
+
+const hamburger = document.getElementById("hamburger");
+const sidebar = document.getElementById("sidebar");
+const overlay = document.getElementById("overlay");
+
+hamburger.addEventListener("click", () => {
+  sidebar.classList.toggle("active");
+  overlay.classList.toggle("active");
+});
+
+overlay.addEventListener("click", () => {
+  sidebar.classList.remove("active");
+  overlay.classList.remove("active");
+});

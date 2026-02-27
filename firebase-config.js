39d7a6a5104a628a565d9b11cd832edfdb55d85a
Farhan Abdurrahman <35007662+syncxcode@users.noreply.github.com>
2026-02-27 15:34:51 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,10 @@
 window.NIHONBYTE_FIREBASE_CONFIG = {
-  apiKey: "YOUR_FIREBASE_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.firebasestorage.app",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
+  apiKey: "AIzaSyBaFQYSr3WlD3h_d-_ZxX63FVCNjLclfSg",
+  authDomain: "nihonbyte.firebaseapp.com",
+  databaseURL: "https://nihonbyte-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "nihonbyte",
+  storageBucket: "nihonbyte.firebasestorage.app",
+  messagingSenderId: "385677455975",
+  appId: "1:385677455975:web:3d8b0fab928c628c501ac2",
+  measurementId: "G-DEL9QGN671"
 };
